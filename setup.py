efec16fa769b80cd039bc05ec966af6cc9b94a68
georges-stephan <georges.stephan@icloud.com>
2021-11-20 21:42:32 +0400

    Added license
    Added license

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='PySwimlanes',
     version='1.1',
+    license='Apache 2.0',
     author='Georges Stephan',
     author_email='georges.stephan@icloud.com',
     packages=find_packages(),
