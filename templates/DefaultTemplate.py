334b40d1b84749526eb20be9378d4382153e9c1b
georges-stephan <georges.stephan@icloud.com>
2022-05-14 12:00:45 +0400

    Added methods to the interface

--- templates/DefaultTemplate.py
+++ templates/DefaultTemplate.py
@@ -27,18 +27,19 @@ class DefaultTemplate(Template):
             'text_margin-right': 4
         }
 
-    def get_parameter_value(self, parameter_name: str):
+    def get_parameter_value(self, parameter_name: str) -> int | str:
         return self.parameters_dict[parameter_name]
 
     def get_font_name_from_font_family_name(self, font_family_name):
         if self.parameters_dict[font_family_name] is None:
             raise TypeError(f'Unknown font family {font_family_name}')
 
-        if self.parameters_dict[font_family_name] == 'sans-serif':
+        if self.parameters_dict[font_family_name].strip().lower() == 'sans-serif':
             if platform.system() == 'Windows':
                 return 'arial'
             else:
                 # TODO also check if we are running on Linux or Mac
+                # TODO check is liberation fonts are installed
                 return 'liberation-sans'
         else:
             # Assuming Serif Font
--- templates/Template.py
+++ templates/Template.py
@@ -2,3 +2,9 @@ class Template:
 
     def __int__(self):
         pass
+
+    def get_parameter_value(self, parameter_name: str) -> int | str:
+        pass
+
+    def get_font_name_from_font_family_name(self, font_family_name) -> str:
+        pass
