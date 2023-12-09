a8ac2623262d501129e47e99c293f2157ddc3b16
georges-stephan <georges.stephan@icloud.com>
2023-12-09 09:45:17 +0400

    Update package versions
    Update package versions

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='PySwimlanes',
-    python_requires='>3.10.0',
-    version='3.3',
+    python_requires='>3.11.4',
+    version='3.5',
     license='Apache 2.0',
     author='Georges Stephan',
     author_email='georges.stephan@icloud.com',
     packages=find_packages(exclude='*_test.py'),
     url='https://github.com/georges-stephan/swimlanes',
-    install_requires=['Pillow>=9.0','pyaml'],
+    install_requires=['Pillow>=10.1.0','PyYAML>=6.0'],
     entry_points={
         'gui_scripts': ['swimlanes=ui.MainUI:main']
     }
